85ef4e426419b085d5c96a2ded0561b4ec1e6e8b
Amir YAZIDI <amir.yazidi1@esprit.tn>
2024-11-11 20:05:42 +0100

    workshop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
 import { ProductComponent } from './product/product.component';
+import { ResidenceComponent } from './residence/residence.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     UserComponent,
-    ProductComponent
+    ProductComponent,
+    ResidenceComponent
   ],
   imports: [
     BrowserModule,
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product',
@@ -8,10 +9,10 @@ import { Component } from '@angular/core';
 export class ProductComponent {
 
   priceMax!:number;
-  Product=[
-    {id:1, title:'iphone 15',price:2500, quantity : 5},
-    {id:2, title:'iphone 16' ,price:3000, quantity : 7},
-    {id:3, title :'iphone14' ,price:3500, quantity :8}
+  Product:Product[]=[
+    {id:1, title:'iphone 15',price:2500, quantity : 5, image:"https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone13pro_09142021_big.jpg.large.jpg"},
+    {id:2, title:'iphone 16' ,price:3000, quantity : 7, image:""},
+    {id:3, title :'iphone14' ,price:3500, quantity :8, image: ""}
      ]
       buy (id:number){
          this.Product.find(p=>p.id==id)!.quantity--;
