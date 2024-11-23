d54fd470cbef49644504cb7d6121482bee21b45f
Amir YAZIDI <amir.yazidi1@esprit.tn>
2024-11-23 14:21:32 +0100

    routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
 import { ProductComponent } from './product/product.component';
 import { ResidenceComponent } from './residence/residence.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { ResidenceComponent } from './residence/residence.component';
     HomeComponent,
     UserComponent,
     ProductComponent,
-    ResidenceComponent
+    ResidenceComponent,
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
